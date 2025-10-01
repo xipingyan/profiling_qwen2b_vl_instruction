9ce0318ca5e7fb06a68a3a6ee8007c9e71d5ab78
xiping.yan <xiping.yan@intel.com>
2025-10-01 11:07:22 +0800

    update pytest script.

--- run_genai_pytest.sh
+++ run_genai_pytest.sh
@@ -1,10 +1,15 @@
 
-workpath=/mnt/xiping/gpu_profiling/profiling_qwen2b_vl_instruction
-cd ${workpath}
+SCRIPT_DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd ${SCRIPT_DIR}
 
 source ./python-env/bin/activate
-# source ./openvino_toolkit_ubuntu22_2025.3.0.dev20250725_x86_64/setupvars.sh
-source /mnt/xiping/gpu_profiling/openvino/build/install/setupvars.sh
+source ./openvino_toolkit_ubuntu24_2025.4.0.dev20250929_x86_64/setupvars.sh
+
+# source /mnt/xiping/gpu_profiling/openvino/build/install/setupvars.sh
+# export INSTALL_DIR=/mnt/xiping/gpu_profiling/openvino/build/install/
+# export OpenVINO_DIR=${INSTALL_DIR}/runtime
+# export PYTHONPATH=${INSTALL_DIR}/python:./build/:$PYTHONPATH
+# export LD_LIBRARY_PATH=${INSTALL_DIR}/runtime/lib/intel64:$LD_LIBRARY_PATH
 
 # Download codes:
 # ================================
@@ -12,12 +17,7 @@ source /mnt/xiping/gpu_profiling/openvino/build/install/setupvars.sh
 # cd openvino.genai/
 # git submodule update --init
 
-export INSTALL_DIR=/mnt/xiping/gpu_profiling/openvino/build/install/
-export OpenVINO_DIR=${INSTALL_DIR}/runtime
-export PYTHONPATH=${INSTALL_DIR}/python:./build/:$PYTHONPATH
-export LD_LIBRARY_PATH=${INSTALL_DIR}/runtime/lib/intel64:$LD_LIBRARY_PATH
-
-GENAI_ROOT_DIR=/mnt/xiping/gpu_profiling/profiling_qwen2b_vl_instruction/openvino.genai/install/python/
+GENAI_ROOT_DIR=${SCRIPT_DIR}/openvino.genai/install/python/
 export PYTHONPATH=${GENAI_ROOT_DIR}:$PYTHONPATH
 export LD_LIBRARY_PATH=${GENAI_ROOT_DIR}/../runtime/lib/intel64/:$LD_LIBRARY_PATH
 
