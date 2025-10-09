8996a7a5a8042338e9c38d80c667d4c90113ac4f
xiping.yan <xiping.yan@intel.com>
2025-10-09 10:56:08 +0800

    test vlm cpu plugin, reuslt is wrong.
    Signed-off-by: xiping.yan <xiping.yan@intel.com>

--- example_ov_py/test_pipeline_genai.py
+++ example_ov_py/test_pipeline_genai.py
@@ -10,22 +10,28 @@ import time
 ov_model='../models/ov/Qwen2.5-VL-3B-Instruct/INT4/'
 
 print("== ov_model=", ov_model)
-device = 'GPU.0'
+device = 'GPU'
+# device = 'CPU'
 print("== device = ", device)
-pipe = ov_genai.VLMPipeline(ov_model, device=device)
+pipe = ov_genai.VLMPipeline(ov_model, device=device, ATTENTION_BACKEND='SDPA')
 
 config = ov_genai.GenerationConfig()
-config.max_new_tokens = 20
+config.max_new_tokens = 50
 
 def load_image(image_url_or_file):
     if str(image_url_or_file).startswith("http") or str(image_url_or_file).startswith("https"):
-        response = requests.get(image_url_or_file)
-        image = Image.open(BytesIO(response.content)).convert("RGB")
+        image = Image.open(requests.get(image_url_or_file, stream=True).raw)
     else:
         image = Image.open(image_url_or_file).convert("RGB")
-    resized_image = image.resize((364, 448), Image.Resampling.BICUBIC)
-    image_data = np.array(resized_image.getdata()).reshape(resized_image.size[1], resized_image.size[0], 3).astype(np.byte)
-    return image, ov.Tensor(image_data)
+    resized_image = image.resize((448, 364), Image.Resampling.BICUBIC)
+    return resized_image
+
+def load_all_images():
+    imgs = []
+    for idx in range(9):
+        image = load_image(f'../test_video/rsz_video/img_{idx}.png')
+        imgs.append(image)
+    return imgs
 
 def streamer(subword: str) -> bool:
     """
@@ -36,7 +42,8 @@ def streamer(subword: str) -> bool:
     print(subword, end="", flush=True)
 
 def test_image():
-    image, image_tensor = load_image('../test_video/img_0.png')
+    image = load_image('../test_video/img_0.png')
+    ov_image = ov.Tensor(image)
     prompt = "请回答以下问题，务必只能回复一个词 \"Y\"或 \"N\"：图片和\"小狗。\"是否相关？"
 
     print(f"Question:\n  {prompt}")
@@ -45,20 +52,22 @@ def test_image():
         t1 = time.time()
         # print("sssss=", type(image_tensor))
         # output = pipe.generate([prompt]*2, image=[image_tensor]*2, generation_config=config)
-        output = pipe.generate(prompt, image=image_tensor, generation_config=config)
+        output = pipe.generate(prompt, image=ov_image, generation_config=config)
         t2 = time.time()
         print(f'== {id} time = {t2-t1:.3f} s')
     print('output = ', output)
-    # print('output.texts = ', output[0].texts, output[1].texts)
-    # print('output[0].scores = ', output[0].scores)
-    # print('output[1].scores = ', output[1].scores)
 
 def test_video(as_video=True):
     print(f"== test video. as_video={as_video}")
-    imgs = []
-    for idx in range(9):
-        image, image_tensor = load_image(f'../test_video/img_{idx}.png')
-        imgs.append(image_tensor)
+
+    images = load_all_images()
+    video = np.stack(images, axis=0)
+
+    print(f"== video shape = {video.shape}")
+    ov_video = ov.Tensor(video)
+    ov_imgs = [ov.Tensor(np.stack([img], axis=0)) for img in images]
+    print(f"== type(ov_imgs) = {type(ov_imgs)}, {ov_imgs[0].data.shape}")
+    
 
     if as_video:
         prompt = "请描述这个视频："
@@ -66,14 +75,14 @@ def test_video(as_video=True):
         prompt = "请描述这些图像："
     print(f"Question:\n  {prompt}")
 
-    for id in range(3):
+    for id in range(1):
         t1 = time.time()
-        pipe.start_chat()
+        # pipe.start_chat()
         if as_video:
-            output = pipe.generate(prompt, video=imgs, generation_config=config)
+            output = pipe.generate(prompt, videos=[ov_video], generation_config=config)
         else:
-            output = pipe.generate(prompt, images=imgs, generation_config=config)
-        pipe.finish_chat()
+            output = pipe.generate(prompt, images=ov_imgs, generation_config=config)
+        # pipe.finish_chat()
         t2 = time.time()
         print(f'== {id} time = {t2-t1:.3f} s')
     print('output = ', output)
@@ -82,4 +91,4 @@ if __name__ == "__main__":
     print("OV Version:", ov.get_version())
     # test_image()
     test_video(as_video=True)
-    test_video(as_video=False)
+    # test_video(as_video=False)
--- example_ov_py/test_pipeline_optimum.py
+++ example_ov_py/test_pipeline_optimum.py
@@ -21,7 +21,7 @@ def load_image(image_url_or_file):
         image = Image.open(requests.get(image_url_or_file, stream=True).raw)
     else:
         image = Image.open(image_url_or_file).convert("RGB")
-    resized_image = image.resize((364, 448), Image.Resampling.BICUBIC)
+    resized_image = image.resize((448, 364), Image.Resampling.BICUBIC)
     return resized_image
 
 def load_all_images():
@@ -59,33 +59,56 @@ def test_image():
     print("== response =", response)
 
 def test_video():
+    max_new_tokens=50
     prompt = "请描述这个视频："
     print("== Test video:")
 
     images = load_all_images()
-    # import numpy as np
-    # print(np.array(images[0]).shape)
+    import numpy as np
+    print(np.array(images[0]).shape)
 
-    inputs = ov_model.preprocess_inputs(text=prompt, video=images, processor=processor)
+    video = np.stack(images, axis=0)
+    print(f"video shape = {video.shape}")
 
-    generation_args = { 
-        "max_new_tokens": 50, 
-        "temperature": 0.0, 
-        "do_sample": False,
-        # "streamer": TextStreamer(processor.tokenizer, skip_prompt=True, skip_special_tokens=True)
-    } 
+    if 0: # from pytest
+        conversation = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "video"},
+                    {"type": "text", "text": prompt},
+                ],
+            }
+        ]
+        templated_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+        inputs = processor(text=[templated_prompt], video=video, padding=True, return_tensors="pt")
+        output_ids = ov_model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=False)
+        input_ids = inputs["input_ids"] if isinstance(inputs, dict) else inputs.input_ids
+        generated_ids = [output_ids[len(input_ids) :] for input_ids, output_ids in zip(input_ids, output_ids)]
 
-    generate_ids = ov_model.generate(**inputs, 
-        eos_token_id=processor.tokenizer.eos_token_id, 
-        **generation_args
-    )
+        optimum_output = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        optimum_text = optimum_output[0]
+        print("== response =", optimum_text)
+    else:# my reference.
+        inputs = ov_model.preprocess_inputs(text=prompt, video=video, processor=processor)
+        generation_args = { 
+            "max_new_tokens": max_new_tokens, 
+            "temperature": 0.0, 
+            "do_sample": False,
+            # "streamer": TextStreamer(processor.tokenizer, skip_prompt=True, skip_special_tokens=True)
+        } 
 
-    generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
-    response = processor.batch_decode(generate_ids, 
-    skip_special_tokens=True, 
-    clean_up_tokenization_spaces=False)[0]
+        generate_ids = ov_model.generate(**inputs, 
+            eos_token_id=processor.tokenizer.eos_token_id, 
+            **generation_args
+        )
 
-    print("== response =", response)
+        generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
+        response = processor.batch_decode(generate_ids, 
+        skip_special_tokens=True, 
+        clean_up_tokenization_spaces=False)[0]
+
+        print("== response =", response)
 
 if __name__ == "__main__":
     print("OV Version:", ov.get_version())
