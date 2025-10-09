4325776a31be02ad2df94e99d69539c0580c6ddc
xiping.yan <xiping.yan@intel.com>
2025-10-09 11:12:36 +0800

    test pass images.
    Signed-off-by: xiping.yan <xiping.yan@intel.com>

--- example_ov_cpp/main.cpp
+++ example_ov_cpp/main.cpp
@@ -83,46 +83,67 @@ void test_images(ov::genai::VLMPipeline &pipe, std::vector<ov::Tensor> rgbs)
 void test_video(ov::genai::VLMPipeline &pipe, std::vector<ov::Tensor> &rgbs, ov::Tensor &video)
 {
     ov::genai::GenerationConfig generation_config;
-    generation_config.max_new_tokens = 2048;
+    // generation_config.max_new_tokens = 2048;
+    generation_config.max_new_tokens = 50;
 
     std::string prompt = "请描述这个视频：";
-    prompt = ""
-             "请基于输入的视频信息（包含视觉画面中的车辆相关元素，如车辆状态、人物与车辆的交互动作、车辆所处场景的道路特征等），完成以下分析任务：首先，精准识别视频中与车辆相关的核心事件或关键现象，包括但不限于人物对车辆的操作行为（如驾驶、停靠、触碰、撞击、移动车辆部件）、车辆自身的状态变化（如剐蹭、变形、故障、位移）、车辆所处场景中的异常情况（如车辆占用应急通道、车辆周边出现障碍物影响行驶、车辆违规停放等）；其次，对识别到的核心内容进行定性判断，明确行为性质（如合规操作 / 违规操作）、风险等级（如无风险 / 低风险 / 高风险）、影响程度（如无影响 / 轻微影响 / 严重影响）或必要措施（如无需提示 / 需提醒用户注意 / 需紧急处理）；最后，将分析结果浓缩为 30 个 token 以内的总结性结论，要求语言简洁、判断明确，避免模糊表述。示例 1：输入视频信息为 “行车记录仪画面显示，一辆黑色 SUV 在路边停靠，一名女子打开右后车门时，车门边缘轻微碰到旁边的白色轿车，白色轿车车身未出现明显划痕，女子未察觉便关上车门离开”，输出结论为 “SUV 车门轻碰白色轿车，无明显损伤，属低风险接触”。示例 2：输入视频信息为 “监控画面中，一名男子站在一辆红色跑车旁，用钥匙在车门表面划动，跑车车门出现一道明显的划痕”，输出结论为 “男子用钥匙划伤跑车车门，造成明显损伤，属高风险破坏，需提醒用户”。示例 3：输入视频信息为 “停车场监控显示，一辆蓝色轿车在倒车时，车尾撞到后方的水泥柱，撞击力度较轻，轿车后保险杠出现轻微变形，无零件脱落”，输出结论为 “蓝色轿车倒车轻撞水泥柱，保险杠微变形，属低风险损伤”。示例 4：输入视频信息为 “路口监控画面中，一辆白色面包车违规停在消防通道上，车身完全占用通道宽度，后司机返回将车开走”，输出结论为 “面包车占用消防通道，无即时影响，需提醒挪车”。示例 5：输入视频信息为 “画面中，一辆电动车在非机动车道行驶，突然变道进入机动车道，与后方驶来的轿车发生轻微剐蹭，电动车后视镜被撞歪，无人员受伤”），输出结论为 “电动车违规变道致剐蹭，轻微损伤，属中风险事故”。示例 7：输入视频信息为 “小区停车场内，一辆银色轿车的左前轮停在绿化带上，碾压了草坪”，输出结论为 “轿车碾压绿化带停车，轻微破坏，需提醒驶离”）。请严格遵循上述要求，基于输入的车辆相关视频信息，必须输出 30token以内的精准结论。结论严格按照json格式{\"事件\"：xxx,\"严重等级\"：xxx ，\"提醒用户\":是或者否}"
-             "";
+    // prompt = ""
+    //          "请基于输入的视频信息（包含视觉画面中的车辆相关元素，如车辆状态、人物与车辆的交互动作、车辆所处场景的道路特征等），完成以下分析任务：首先，精准识别视频中与车辆相关的核心事件或关键现象，包括但不限于人物对车辆的操作行为（如驾驶、停靠、触碰、撞击、移动车辆部件）、车辆自身的状态变化（如剐蹭、变形、故障、位移）、车辆所处场景中的异常情况（如车辆占用应急通道、车辆周边出现障碍物影响行驶、车辆违规停放等）；其次，对识别到的核心内容进行定性判断，明确行为性质（如合规操作 / 违规操作）、风险等级（如无风险 / 低风险 / 高风险）、影响程度（如无影响 / 轻微影响 / 严重影响）或必要措施（如无需提示 / 需提醒用户注意 / 需紧急处理）；最后，将分析结果浓缩为 30 个 token 以内的总结性结论，要求语言简洁、判断明确，避免模糊表述。示例 1：输入视频信息为 “行车记录仪画面显示，一辆黑色 SUV 在路边停靠，一名女子打开右后车门时，车门边缘轻微碰到旁边的白色轿车，白色轿车车身未出现明显划痕，女子未察觉便关上车门离开”，输出结论为 “SUV 车门轻碰白色轿车，无明显损伤，属低风险接触”。示例 2：输入视频信息为 “监控画面中，一名男子站在一辆红色跑车旁，用钥匙在车门表面划动，跑车车门出现一道明显的划痕”，输出结论为 “男子用钥匙划伤跑车车门，造成明显损伤，属高风险破坏，需提醒用户”。示例 3：输入视频信息为 “停车场监控显示，一辆蓝色轿车在倒车时，车尾撞到后方的水泥柱，撞击力度较轻，轿车后保险杠出现轻微变形，无零件脱落”，输出结论为 “蓝色轿车倒车轻撞水泥柱，保险杠微变形，属低风险损伤”。示例 4：输入视频信息为 “路口监控画面中，一辆白色面包车违规停在消防通道上，车身完全占用通道宽度，后司机返回将车开走”，输出结论为 “面包车占用消防通道，无即时影响，需提醒挪车”。示例 5：输入视频信息为 “画面中，一辆电动车在非机动车道行驶，突然变道进入机动车道，与后方驶来的轿车发生轻微剐蹭，电动车后视镜被撞歪，无人员受伤”），输出结论为 “电动车违规变道致剐蹭，轻微损伤，属中风险事故”。示例 7：输入视频信息为 “小区停车场内，一辆银色轿车的左前轮停在绿化带上，碾压了草坪”，输出结论为 “轿车碾压绿化带停车，轻微破坏，需提醒驶离”）。请严格遵循上述要求，基于输入的车辆相关视频信息，必须输出 30token以内的精准结论。结论严格按照json格式{\"事件\"：xxx,\"严重等级\"：xxx ，\"提醒用户\":是或者否}"
+    //          "";
     std::cout << "  prompt: " << prompt << std::endl;
 
-    // std::cout << "  test_video pass 'video' with multiple tensors " << std::endl;
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     pipe.start_chat();
-    //     std::cout << "  Loop: [" << i << "] ";
-    //     auto t1 = std::chrono::high_resolution_clock::now();
-    //     auto aa = pipe.generate(prompt,
-    //                             ov::genai::videos(rgbs),
-    //                             ov::genai::generation_config(generation_config));
-    //     // ov::genai::streamer(print_subword));
-    //     auto t2 = std::chrono::high_resolution_clock::now();
-    //     std::cout << ", result: text =" << aa.texts[0].c_str() << ", score=" << aa.scores[0] << ", tm=" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
-    //     pipe.finish_chat();
-    // }
+#if 1
+    std::cout << "  test_video pass 'images' " << std::endl;
+    for (int i = 0; i < 1; i++)
+    {
+        pipe.start_chat();
+        std::cout << "  Loop: [" << i << "] ";
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto aa = pipe.generate(prompt,
+                                ov::genai::images(rgbs),
+                                ov::genai::generation_config(generation_config));
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::cout << ", result: text =" << aa.texts[0].c_str() << ", score=" << aa.scores[0] << ", tm=" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
+        pipe.finish_chat();
+    }
+#endif
 
-    // std::cout << "  test_video pass 'video' with one tensor " << std::endl;
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     pipe.start_chat();
-    //     std::cout << "  Loop: [" << i << "] ";
-    //     auto t1 = std::chrono::high_resolution_clock::now();
-    //     auto aa = pipe.generate(prompt,
-    //                             ov::genai::videos(std::vector<ov::Tensor>{video}),
-    //                             ov::genai::generation_config(generation_config));
-    //     // ov::genai::streamer(print_subword));
-    //     auto t2 = std::chrono::high_resolution_clock::now();
-    //     std::cout << ", result: text =" << aa.texts[0].c_str() << ", score=" << aa.scores[0] << ", tm=" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
-    //     pipe.finish_chat();
-    // }
+#if 0
+    std::cout << "  test_video pass 'video' with multiple tensors " << std::endl;
+    for (int i = 0; i < 1; i++)
+    {
+        pipe.start_chat();
+        std::cout << "  Loop: [" << i << "] ";
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto aa = pipe.generate(prompt,
+                                ov::genai::videos(rgbs),
+                                ov::genai::generation_config(generation_config));
+        // ov::genai::streamer(print_subword));
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::cout << ", result: text =" << aa.texts[0].c_str() << ", score=" << aa.scores[0] << ", tm=" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
+        pipe.finish_chat();
+    }
+#endif
 
+#if 0
+    std::cout << "  test_video pass 'video' with one tensor " << std::endl;
+    for (int i = 0; i < 1; i++)
+    {
+        pipe.start_chat();
+        std::cout << "  Loop: [" << i << "] ";
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto aa = pipe.generate(prompt,
+                                ov::genai::videos(std::vector<ov::Tensor>{video}),
+                                ov::genai::generation_config(generation_config));
+        // ov::genai::streamer(print_subword));
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::cout << ", result: text =" << aa.texts[0].c_str() << ", score=" << aa.scores[0] << ", tm=" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
+        pipe.finish_chat();
+    }
+#endif
+
+#if 0
     std::cout << "  test_video pass 'video' + 'images' " << std::endl;
-
     std::vector<ov::Tensor> images = std::vector<ov::Tensor>{rgbs[0], rgbs[1]};
     auto shape = rgbs[0].get_shape();
     ov::Tensor video1 = ov::Tensor(ov::element::u8, ov::Shape({3, shape[1], shape[2], shape[3]}));
@@ -149,6 +170,7 @@ void test_video(ov::genai::VLMPipeline &pipe, std::vector<ov::Tensor> &rgbs, ov:
         std::cout << "      result: text =" << aa.texts[0].c_str() << ", score=" << aa.scores[0] << ", tm=" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
         pipe.finish_chat();
     }
+#endif
 }
 
 int main(int argc, char *argv[])
@@ -175,7 +197,7 @@ int main(int argc, char *argv[])
         ov::Tensor video = utils::load_video(img_video_path);
 
         std::cout << "== Start to load model: " << model_path << std::endl;
-        cfg["ATTENTION_BACKEND"] = "PA";
+        cfg["ATTENTION_BACKEND"] = "SDPA";
         ov::genai::VLMPipeline pipe(model_path, device, cfg);
 
         if (input_video)
--- example_ov_cpp/my_run.sh
+++ example_ov_cpp/my_run.sh
@@ -13,6 +13,7 @@ type="video"
 video_img_path=../test_video/rsz_video
 
 device='GPU'
+device='CPU'
 
 # gdb --args 
 # onetrace --chrome-call-logging --chrome-device-timeline 
