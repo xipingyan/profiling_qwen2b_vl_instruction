037bfa117a9873d925cf208093ec27122c510168
xipingya <xiping.yan@intel.com>
2025-11-20 14:31:34 +0800

    x

--- example_ov_cpp/my_run.sh
+++ example_ov_cpp/my_run.sh
@@ -7,16 +7,17 @@ source ../source_ov.sh
 cd ${SCRIPT_DIR_EXAMPLE_OV_CPP_RUN}
 
 model_id=../models/ov/Qwen2.5-VL-3B-Instruct/INT4/
-model_id=../openvino.genai/tests/python_tests/ov_cache/20251015/optimum-intel-1.25.2_transformers-4.53.3/test_models/katuni4ka_tiny-random-qwen2.5-vl/
+# model_id=../openvino.genai/tests/python_tests/ov_cache/20251015/optimum-intel-1.25.2_transformers-4.53.3/test_models/katuni4ka_tiny-random-qwen2.5-vl/
 
-# type="img"
-# video_img_path=../openvino.genai/tests/python_tests/.pytest_cache/d/images/handwritten.png
+type="img"
+video_img_path=../openvino.genai/tests/python_tests/.pytest_cache/d/images/handwritten.png
+video_img_path="../test_video/home.jpg"
 
-type="video"
-video_img_path=../test_video/rsz_video
+# type="video"
+# video_img_path=../test_video/rsz_video
 
 device='GPU'
-device='CPU'
+# device='CPU'
 
 # gdb --args 
 # onetrace --chrome-call-logging --chrome-device-timeline 
