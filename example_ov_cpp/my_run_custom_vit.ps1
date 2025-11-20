7b32f9a0a9fa63f7e57a7981bdbb9ee502156b2d
Yan <xiping.yan@intel.com>
2025-11-20 19:30:43 +0800

    update test.

--- example_ov_cpp/my_run_custom_vit.ps1
+++ example_ov_cpp/my_run_custom_vit.ps1
@@ -15,10 +15,10 @@ $env:model_id = "../models/ov/Qwen2.5-VL-3B-Instruct/INT4/"
 
 $env:type="img"
 $env:prompt = "prompt.txt"
-echo "Please describe the image." > $prompt
+# echo "Please describe the image." > $env:prompt
 
 $env:prompt2 = "prompt2.txt"
-echo "How many chars in this image?" > $prompt2
+# echo "How many chars in this image?" > $env:prompt2
 
 $env:device='GPU'
 $env:GENAI_ROOT_DIR = "$env:SCRIPT_DIR_EXAMPLE_OV_CPP_RUN\\.."
@@ -29,6 +29,7 @@ echo "== Start: qwen2vl_app_cpp"
 
 $env:CUSTOM_VIT_PATH = "C:\\ov_task\\profiling_qwen2b_vl_instruction\\custom_vit"
 $env:CUSTOM_VIT_IMG_PATH = "C:\\ov_task\\profiling_qwen2b_vl_instruction\\custom_vit\\home.jpg"
-$env:ENABLE_CUSTOM_VIT = "1"
+$env:ENABLE_CUSTOM_VIT = "0"
 
-.\\build\\Debug\\qwen2vl_app_cpp.exe $env:model_id $env:type $env:CUSTOM_VIT_IMG_PATH $env:device $env:prompt $env:prompt2
+# .\\build\\Debug\\qwen2vl_app_cpp.exe $env:model_id $env:type $env:CUSTOM_VIT_IMG_PATH $env:device $env:prompt $env:prompt2
+.\\build\\Release\\qwen2vl_app_cpp.exe $env:model_id $env:type $env:CUSTOM_VIT_IMG_PATH $env:device $env:prompt $env:prompt2
