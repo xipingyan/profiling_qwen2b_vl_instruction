6244db9383f90b749dbf72dc69eb32dc4d06a755
xipingya <xiping.yan@intel.com>
2025-11-21 13:29:49 +0800

    change ov to 26.0

--- source_ov.sh
+++ source_ov.sh
@@ -6,8 +6,7 @@ source ./python-env/bin/activate
 
 OS_VERSION=$(lsb_release -rs)
 if [ "$OS_VERSION" = "22.04" ]; then
-    # source ./openvino_toolkit_ubuntu22_2026.0.0.dev20251110_x86_64/setupvars.sh
-    source ./openvino_toolkit_ubuntu22_2025.4.0.dev20251105_x86_64/setupvars.sh
+    source ./openvino_toolkit_ubuntu22_2026.0.0.dev20251110_x86_64/setupvars.sh
 elif [ "$OS_VERSION" = "24.04" ]; then
     source ./openvino_toolkit_ubuntu24_2025.4.0.dev20251017_x86_64/setupvars.sh
 elif [ "$OS_VERSION" = "24.10" ]; then
