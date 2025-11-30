738a05d91d859eaffaeb33f9c6e63886f8ad4f23
xipingya <xiping.yan@intel.com>
2025-11-30 17:36:45 +0800

    eagle3 py example.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- example_ov_py/run.sh
+++ example_ov_py/run.sh
@@ -1,17 +1,19 @@
-SCRIPT_DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
-cd ${SCRIPT_DIR}
+SCRIPT_DIR_OV_PY="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd ${SCRIPT_DIR_OV_PY}
 
 # PYTHON API test: openvino/genai need to share same python ENV.
 source ../python-env/bin/activate
 source ../source_ov.sh
-cd ${SCRIPT_DIR}
+cd ${SCRIPT_DIR_OV_PY}
 
 GENAI_ROOT_DIR=../openvino.genai/install/python/
 export PYTHONPATH=${GENAI_ROOT_DIR}:$PYTHONPATH
 export LD_LIBRARY_PATH=${GENAI_ROOT_DIR}/../runtime/lib/intel64/:$LD_LIBRARY_PATH
 
 # gdb --args 
-python ./test_pipeline_genai.py
+# python ./test_pipeline_genai.py
+python ./test_pipeline_genai_eagle3.py
+
 # python ./test_pipeline_optimum.py
 # python test_llm_genai.py
 # python test_vlm_continuous_batching_generate_vs_add_request.py
