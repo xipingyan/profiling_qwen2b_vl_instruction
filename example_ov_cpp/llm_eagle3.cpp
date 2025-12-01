554914e5539da72f989d75f706dce8e8744dabdc
xiping.yan <xiping.yan@intel.com>
2025-12-01 10:47:12 +0800

    print eagle3 performance
    Signed-off-by: xiping.yan <xiping.yan@intel.com>

--- example_ov_cpp/llm_eagle3.cpp
+++ example_ov_cpp/llm_eagle3.cpp
@@ -5,7 +5,26 @@
 #include <openvino/genai/visual_language/pipeline.hpp>
 #include <filesystem>
 #include <chrono>
-
+#include "openvino/genai/speculative_decoding/perf_metrics.hpp"
+template <typename T>
+void print_perf_metrics(T& perf_metrics, std::string model_name) {
+    std::cout << "\n" << model_name << std::endl;
+    auto generation_duration = perf_metrics.get_generate_duration().mean;
+    std::cout << "  Generate time: " << generation_duration << " ms" << std::endl;
+    std::cout << "  TTFT: " << perf_metrics.get_ttft().mean << " ± " << perf_metrics.get_ttft().std << " ms"
+              << std::endl;
+    std::cout << "  TPOT: " << perf_metrics.get_tpot().mean << " ± " << perf_metrics.get_tpot().std << " ms/token"
+              << std::endl;
+    std::cout << "  Num generated token: " << perf_metrics.get_num_generated_tokens() << " tokens" << std::endl;
+    if (model_name == "Total") {
+        std::cout << "  Total iteration number: " << perf_metrics.raw_metrics.m_new_token_times.size() << std::endl;
+    } else {
+        std::cout << "  Total iteration number: " << perf_metrics.raw_metrics.m_durations.size() << std::endl;
+    }
+    if (perf_metrics.get_num_input_tokens() > 0) {
+        std::cout << "  Input token size: " << perf_metrics.get_num_input_tokens() << std::endl;
+    }
+}
 int test_vllm_eagle3(int argc, char* argv[]) {
     std::cout << "== test_vllm_eagle3 ..." << std::endl;
     ov::AnyMap enable_compile_cache;
@@ -30,6 +49,7 @@ int test_vllm_eagle3(int argc, char* argv[]) {
     config.max_new_tokens = 256;
     config.do_sample=false;
     config.temperature=0.1;
+    config.num_assistant_tokens = 5; // eagle3 param.
 
     auto draft_model = ov::genai::draft_model(draft_path, device);
 
@@ -52,7 +72,7 @@ int test_vllm_eagle3(int argc, char* argv[]) {
     std::string prompts = "Please describe this image.";
     prompts = "简单描述一下这张图";
 
-    for (size_t i = 0;i < 4; i++) {
+    for (size_t i = 0;i < 8; i++) {
         std::cout << "== Loop: " << i << std::endl;
         // pipe.start_chat();
         auto t1 = std::chrono::high_resolution_clock::now();
@@ -63,6 +83,18 @@ int test_vllm_eagle3(int argc, char* argv[]) {
         std::cout << " outputs: " << outputs << '\n';
         std::cout << " TTFT: " << outputs.perf_metrics.get_ttft().mean << '\n';
         std::cout << " TPOT: " << outputs.perf_metrics.get_tpot().mean << '\n';
+
+        auto sd_perf_metrics = std::dynamic_pointer_cast<ov::genai::SDPerModelsPerfMetrics>(outputs.extended_perf_metrics);
+        print_perf_metrics(outputs.perf_metrics, "Total");
+        if (sd_perf_metrics) {
+            print_perf_metrics(sd_perf_metrics->main_model_metrics, "MAIN MODEL");
+            std::cout << "  accepted token: " << sd_perf_metrics->get_num_accepted_tokens() << " tokens" << std::endl;
+            std::cout << "  compress rate: "
+                    << sd_perf_metrics->main_model_metrics.get_num_generated_tokens() * 1.0f /
+                            sd_perf_metrics->main_model_metrics.raw_metrics.m_durations.size()
+                    << std::endl;
+            print_perf_metrics(sd_perf_metrics->draft_model_metrics, "DRAFT MODEL");
+        }
     }
     return 0;
 }
