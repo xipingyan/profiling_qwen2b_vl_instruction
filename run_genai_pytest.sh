337a28cbced952c72878192b791fea5f0693fff9
xipingya <xiping.yan@intel.com>
2026-01-20 09:28:05 +0800

    python_test, local test pass.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- run_genai_pytest.sh
+++ run_genai_pytest.sh
@@ -4,6 +4,7 @@ cd ${SCRIPT_RUN_GENAI_PYTEST_DIR}
 
 source ./python-env/bin/activate
 # pip install --pre -U openvino openvino-tokenizers==2025.4.0.0.dev20250929 --extra-index-url https://storage.openvinotoolkit.org/simple/wheels/nightly
+# pip install -r openvino.genai/tests/python_tests/requirements.txt
 source ./source_ov.sh
 cd ${SCRIPT_RUN_GENAI_PYTEST_DIR}
 
@@ -21,14 +22,18 @@ cd ${SCRIPT_RUN_GENAI_PYTEST_DIR}
 
 GENAI_ROOT_DIR=${SCRIPT_RUN_GENAI_PYTEST_DIR}/openvino.genai/install/python/
 export PYTHONPATH=${GENAI_ROOT_DIR}:$PYTHONPATH
-export LD_LIBRARY_PATH=${GENAI_ROOT_DIR}../runtime/lib/intel64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${GENAI_ROOT_DIR}/../runtime/lib/intel64/:$LD_LIBRARY_PATH
+
+# Ensure the Python openvino_tokenizers package uses the GenAI-built extension
+# (must be ABI-compatible with the OpenVINO runtime used by setupvars.sh).
+export OV_TOKENIZER_PREBUILD_EXTENSION_PATH=${GENAI_ROOT_DIR}/../runtime/lib/intel64/libopenvino_tokenizers.so
 
 export HF_ENDPOINT=https://hf-mirror.com
 
 cd openvino.genai/tests/python_tests
 export OV_CACHE=./ov_cache
 # python -m pytest ./ -s -m precommit -k test_vlm_continuous_batching_generate_vs_add_request
-# python -m pytest ./ -s -m precommit -k test_vlm_pipeline_match_optimum_preresized
+# python -m pytest ./ -s -k test_stop_strings_facebook_opt
 #  gdb --args python -m pytest ./ -s -m precommit -k test_vlm_pipeline_chat_with_video
 
 # python -m pytest ./ -m precommit -k test_add_extension
